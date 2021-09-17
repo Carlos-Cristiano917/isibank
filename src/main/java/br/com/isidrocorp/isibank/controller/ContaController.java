8f65b0e1bdf51a050ef369592f89acbade945a41
Carlos-Cristiano917 <carloscristiano.csp@gmail.com>
2021-09-17 15:23:08 -0300

    Projeto finalizado

---
+++ src/main/java/br/com/isidrocorp/isibank/controller/ContaController.java
@@ -0,0 +1,31 @@
+package br.com.isidrocorp.isibank.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.isidrocorp.isibank.dao.ContaDAO;
+import br.com.isidrocorp.isibank.model.Conta;
+
+@RestController
+public class ContaController {
+	@Autowired
+	private ContaDAO dao;
+
+	@GetMapping("/contas")
+	public ArrayList<Conta> recuperartodos() {
+		return (ArrayList<Conta>) dao.findAll();
+	}
+	@GetMapping("/contas/{numero}")
+	public ResponseEntity<Conta> recuperarPelonumero(@PathVariable int numero) {
+		Conta resultado = dao.findById(numero).orElse(null);
+		if (resultado != null) {
+			return ResponseEntity.ok(resultado);
+		}
+		return ResponseEntity.notFound().build();
+	}
+}
